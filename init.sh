a5a6465ff341a5908a2c149091d94c0c02642f71
Lawrence as Developer <smartlitchi@gmail.com>
2014-12-04 02:10:43 +0800

    Enhance to create a link from parent folder to .builder to avoid need of -f .builder/build.xml when run ant

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@ cp -R projectTemplate/* ../
 mv ../project.iml.sample ../project.iml
 mv ../project.ipr.sample ../project.ipr
 mv ../project.iws.sample ../project.iws
+cd ..
+ln -s .builder/build.xml build.xml
